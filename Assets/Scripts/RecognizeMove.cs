800e2538c59f1e827ddb430b0bf3127f82b9174a
KayleeLChan <kayleel.chan@gmail.com>
2021-06-14 23:13:33 -0700

    Finished Ocean world

--- Assets/Scripts/RecognizeMove.cs
+++ Assets/Scripts/RecognizeMove.cs
@@ -8,14 +8,14 @@ public class RecognizeMove : MonoBehaviour
     public XRNode inputSource;
     public InputHelpers.Button inputButton;
     public float inputThreshold = 0.1f;
+    public Transform movementSource;
+    public AudioSource audioSource;
+
+    public float newPositionThresholdDistance = 0.05f;
+    public GameObject debugCubePrefab;
 
     private bool isMoving = false;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private List<Vector3> positionsList = new List<Vector3>();
 
     // Update is called once per frame
     void Update()
@@ -43,6 +43,10 @@ public class RecognizeMove : MonoBehaviour
     {
         Debug.Log("Start Movement");
         isMoving = true;
+        positionsList.Clear();
+        positionsList.Add(movementSource.position);
+        Instantiate(debugCubePrefab, movementSource.position, Quaternion.identity);
+        audioSource.Play();
     }
 
     void EndMovement()
@@ -54,5 +58,13 @@ public class RecognizeMove : MonoBehaviour
     void UpdateMovement()
     {
         Debug.Log("Update Movement");
+        Vector3 lastPosition = positionsList[positionsList.Count - 1];
+
+        if (Vector3.Distance(movementSource.position, lastPosition) > newPositionThresholdDistance)
+        {
+            positionsList.Add(movementSource.position);
+            Destroy(Instantiate(debugCubePrefab, movementSource.position, Quaternion.identity), 7);
+            audioSource.Play();
+        }
     }
 }
